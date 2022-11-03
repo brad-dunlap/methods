7120e2da0a1e0c17fd019cf7b05f6856189515b8
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-03 15:57:57 -0400

    Add final_practice

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,73 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
-# What is the return value of your method?
+def greeting
+    puts "Hello fine human!"
+end
+
+greeting
+
+# What is the return value of your method? 
+## Hello fine human!
 # How many arguments did you pass your method?
+## 0
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def custom_greeting(name)
+    "Hello, #{name}!"
+end
+
+puts custom_greeting("Brad")
+
+
 
 # What is the return value of your method?
+## Hello, Brad!
 # How many arguments did you pass your method?
+## 1
 # What data type was your argument(s)?
+## String
 
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
 
+def square(num)
+   puts num*num
+end
 
+square(3)
 
-# What is the return value of your method?
+# What is the return value of your method? 
+## the square of the number, 9
+
 # How many arguments did you pass your method?
+## 1
 # What data type was your argument(s)?
-
+## integer  
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
 
+def greet_person2(fname, mname, lname)
+    "Hello, #{fname} #{mname} #{lname}"
+end
+
+
+p greet_person2("Bradley", "Allen", "Dunlap")
 
 # What is the return value of your method?
+## "Hello, Bradley Allen Dunlap"
+
 # How many arguments did you pass your method?
+## 3
 # What data type was your argument(s)?
+## String
 
--- methods_variables.rb
+++ methods_variables.rb
@@ -8,7 +8,12 @@
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
 
+name = "Brad"
 
+p name.upcase
+p name.downcase
+p name.reverse
+p name.length
 
 
 
@@ -23,10 +28,33 @@ last_login = "12/09/2021"
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
+# This method will upcase all of the characters in the defined variable
+p user_name.upcase!
+
+# This method separates each character in the string  with a given block
+p user_name.each_char {|c| print c, ' ' }
+
+#This method reverses all the characters in a given string.
+p user_name.reverse
+
+#This method returns the first character of a given string. 
+p user_name.chr
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
 
+# In methods, the bang is usually an indicator to "be careful", but more specifically it shows that 
+# a method has been modified in some way and you are going to run the modified version. 
+# It's a warning to the programmer that something unexpected can happen
+
+# In the case below, adding the bang modified the method changes the output from "answer" to "Answer" because
+# the former one returns the original a where as the bang version uses the modified variable prior to it's output. 
+a = "answer"
+a.capitalize
+puts a
+
+a.capitalize!
+puts a
 
 
 
